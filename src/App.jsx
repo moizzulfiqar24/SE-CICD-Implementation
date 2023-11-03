c73662e50960fccfcb0840900ad0b478ed114523
Moiz Zulfiqar <moiz.zulfiqar@icloud.com>
2023-11-03 20:00:26 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>SE CICD Assignment Implementation</h2>
+      <h2>SE CI/CD Assignment Implementation</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
